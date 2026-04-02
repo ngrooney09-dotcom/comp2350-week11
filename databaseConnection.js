f30c26567fa5a3575e2bae5bb0a6e79467077ca4
Long Nguyen <qnguyen78@my.bcit.ca>
2026-04-01 21:14:38 -0700

    ll

--- databaseConnection.js
+++ databaseConnection.js
@@ -2,7 +2,7 @@ const MongoClient = require("mongodb").MongoClient;
 
 const is_hosted = process.env.IS_HOSTED === "true";
 
-const hostedURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const hostedURI = "mongodb+srv://ngrooney09_db_user:86lftdMKrXlCpEV1@cluster0.wowgtvj.mongodb.net/?retryWrites=true&w=majority";
 const localURI = "mongodb://127.0.0.1/?authSource=admin&retryWrites=true&w=majority";
 
 const database = new MongoClient(is_hosted ? hostedURI : localURI);
