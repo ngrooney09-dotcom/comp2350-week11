ab36a77e25bffa07082072effb42f8d825181e78
Long Nguyen <qnguyen78@my.bcit.ca>
2026-04-01 21:01:16 -0700

    fadfds

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,11 +1,16 @@
 const MongoClient = require("mongodb").MongoClient;
+
 const is_hosted = process.env.IS_HOSTED || false;
-const hostedURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-const localURI = "mongodb://127.0.0.1/?authSource=admin&retryWrites=true&w=majority"
+
+const hostedURI = "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const localURI = "mongodb://127.0.0.1/?authSource=admin&retryWrites=true&w=majority";
+
+let database;
+
 if (is_hosted) {
-var database = new MongoClient(hostedURI, {useNewUrlParser: true, useUnifiedTopology: true});
+  database = new MongoClient(hostedURI);
+} else {
+  database = new MongoClient(localURI);
 }
-else {
-var database = new MongoClient(localURI, {useNewUrlParser: true, useUnifiedTopology: true});
-}
+
 module.exports = database;
